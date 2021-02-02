6cec6fe6a883a043a016526f0161acff5b80b990
Liliya Artyukh <lilyart@Liliyas-MacBook-Pro.local>
2021-02-01 22:40:16 -0500

    example using connect

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
-// the http module is used to create a small web server
-const http = require('http');
+// The application file is using the connect module to create a new web server.
+// Connect isn't a core module, so you'll have to install it using npm (npm install connect). 
+const connect = require('connect');
+const app = connect();
 
-// basic example of a basic Node web server
-// Notice the callback function that is passed as an argument to the createServer() method.
-http.createServer((req, res) => {res.writeHead(200, {'Content-Type': 'text/plain'});
+// Connect middlewareConnect middleware is basically a JavaScript function with a unique signature. 
+// Each middleware function is defined with the following three arguments: 
+// req: This is an object that holds the HTTP requestinformation 
+// res: This is an object that holds the HTTP responseinformation and allows you to set the response properties 
+// next: This is the next middleware function defined in the ordered set of Connect middleware
+function helloWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+};
 
-// The listen() method is then used to listen to the 3000 port.
-res.end('Hello World');}).listen(3000);
+function goodbyeWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Goodbye World');
+};
 
+function logger(req, res, next) {
+    console.log(req.method, req.url)
+    next();
+};
 
-// Now, open http://localhost:3000 in your browser and you'll see the Hello World response.
+// When you have a middleware defined, you'll just have to register it with the Connect application using the app.use() method. 
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbyeWorld);
+
+app.listen(3000);
+
 console.log('Server running at http://localhost:3000/');
