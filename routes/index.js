71f912cbc36da593c4e09e28d2f8848b38acd4c6
Liliya Artyukh <lilyart@Liliyas-MacBook-Pro.local>
2021-02-02 23:46:57 -0500

    express with bootstrap and some other packages

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,47 @@ let router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', 
   { 
-    title: 'My App',
-    subTitle: 'My Portfolio'
+    title: 'Home'
+  });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', 
+  { 
+    title: 'Home'
+  });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', 
+  { 
+    title: 'About'
+  });
+});
+
+/* GET products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', 
+  { 
+    title: 'Products'
+  });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', 
+  { 
+    title: 'Services'
+  });
+});
+
+/* GET contact us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', 
+  { 
+    title: 'Contact Us'
   });
 });
 
