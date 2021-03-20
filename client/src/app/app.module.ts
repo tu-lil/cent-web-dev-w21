1ac48bb19af7b43167e94f7646a5f8950a1d914a
Liliya Artyukh <lilyart@Liliyas-MacBook-Pro.local>
2021-03-20 13:52:19 -0400

    cleaned up some bookstore component and module code

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,15 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BookstoreModule } from './bookstore/bookstore.module'
+
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
+
 import { HomeComponent } from './pages/home/home.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { ProductsComponent } from './pages/products/products.component';
-import { BookstoreComponent } from './bookstore/bookstore.component'; 
 
 // decorator
 @NgModule({
@@ -23,12 +26,12 @@ import { BookstoreComponent } from './bookstore/bookstore.component';
     ServicesComponent,
     AboutComponent,
     ContactUsComponent,
-    ProductsComponent,
-    BookstoreComponent 
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BookstoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/bookstore/bookstore.module.ts
+++ client/src/app/bookstore/bookstore.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BookstoreComponent } from './bookstore.component'
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BookstoreComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    BookstoreComponent
   ]
 })
 export class BookstoreModule { }
